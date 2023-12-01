963c1cf3dde15b63bbf8716d0779b45264465bdf
kierth <villegaskierthryan@gmail.com>
2023-12-01 22:42:45 +0800

    multiple

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-require "pages/home.php"
+function require_multi($files) {
+    $files = func_get_args();
+    foreach($files as $file)
+        require_once($file);
+}
+require_multi("pages/home.php","includes/footer.php","includes/navbar.php");
 ?>
