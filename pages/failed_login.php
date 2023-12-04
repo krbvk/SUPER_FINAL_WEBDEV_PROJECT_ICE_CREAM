0a554dd8486b06219a5fde1231d9c18a6ed1890f
kierth <villegaskierthryan@gmail.com>
2023-12-04 16:33:50 +0800

    added favicon only on viewable pages

--- pages/failed_login.php
+++ pages/failed_login.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../assets/images/favicon-16x16.png" type="image/png">
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon" sizes="16x16">
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon" sizes="32x32">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/home.css">
     <link rel="stylesheet" href="../assets/css/forms.css">
