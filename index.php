41d7929f2a2857f78069f9337769e244678229aa
kierth <villegaskierthryan@gmail.com>
2023-12-04 16:36:06 +0800

    initial commit

--- index.php
+++ index.php
@@ -6,3 +6,16 @@ function require_multi($files) {
 }
 require_multi("includes/navbar.php","pages/home.php","includes/footer.php",);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atelier De Natsu</title>
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../assets/images/favicon-16x16.png" type="image/png">
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon" sizes="16x16">
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon" sizes="32x32">
+</head>
+</html>
