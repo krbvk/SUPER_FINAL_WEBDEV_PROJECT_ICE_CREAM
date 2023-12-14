e1ae9baf00bfc1ea75f98c950c4826108815522a
kierth <146813603+krbvk@users.noreply.github.com>
2023-12-14 22:23:04 +0800

    develop

--- pages/summary.php
+++ pages/summary.php
@@ -19,6 +19,7 @@
 <body>
     <?php include '../includes/navbar.php'; ?>
     <div class="container">
+    <form method ="GET" action="/summary_auth.php">
         <h1>Summary</h1>
         <table class="table table-striped">
             <thead>
@@ -106,6 +107,7 @@
             sessionStorage.removeItem('cartItems');
         });
     </script>
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
