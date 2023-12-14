14b1013713284f03244785944c3d93e489e62ede
kierth <146813603+krbvk@users.noreply.github.com>
2023-12-14 22:43:23 +0800

    help me

--- pages/summary.php
+++ pages/summary.php
@@ -19,7 +19,6 @@
 <body>
     <?php include '../includes/navbar.php'; ?>
     <div class="container">
-    <form method ="POST" action="../pages/receipt.php">
         <h1>Summary</h1>
         <table class="table table-striped">
             <thead>
@@ -107,7 +106,6 @@
             sessionStorage.removeItem('cartItems');
         });
     </script>
-    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
